d889e4d067e123ece727887f55200956e583fa91
Christian Ang <cang@pivotal.io>
2017-08-11 11:28:42 -0700

    Symlink repo into gopath
    - in prepare-workspace
    
    [#150156718]

--- ci/tasks/prepare-workspace/task.sh
+++ ci/tasks/prepare-workspace/task.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -exu
 
 ROOT=${PWD}
+GOPATH=${PWD}/go
+PATH=${GOPATH}/bin:$PATH
 
 function main() {
-  pushd "${ROOT}/azure-blobstore-resource" > /dev/null
+  mkdir -p "${GOPATH}/src/github.com/pivotal-cf"
+  ln -s "${ROOT}/azure-blobstore-resource" "${GOPATH}/src/github.com/pivotal-cf/azure-blobstore-resource"
+  pushd "${GOPATH}/src/github.com/pivotal-cf/azure-blobstore-resource" > /dev/null
     ./scripts/build
     echo "release_candidate" > tag
   popd > /dev/null

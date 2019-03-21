e5b67a828997cb33a97885ec712f710c91355e80
Christian Ang <cang@pivotal.io>
2019-03-20 17:52:12 -0700

    Update prepare-workspace for go mod
    - no longer need to symlink to GOPATH
    - no longer need to set GOPATH

--- ci/tasks/prepare-workspace/task.sh
+++ ci/tasks/prepare-workspace/task.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -exu
 
 ROOT=${PWD}
-GOPATH=${PWD}/go
-PATH=${GOPATH}/bin:$PATH
 
 function main() {
-  mkdir -p "${GOPATH}/src/github.com/pivotal-cf"
-  ln -s "${ROOT}/azure-blobstore-resource" "${GOPATH}/src/github.com/pivotal-cf/azure-blobstore-resource"
-  pushd "${GOPATH}/src/github.com/pivotal-cf/azure-blobstore-resource" > /dev/null
+  pushd "${ROOT}/azure-blobstore-resource" > /dev/null
     ./scripts/build
   popd > /dev/null
 

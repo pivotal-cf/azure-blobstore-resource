250c0e31f3b981d2ca99f8d3b1daed792606e683
Christian Ang <cang@pivotal.io>
2019-03-20 17:38:08 -0700

    Update run-test script for go mod
    - no longer need to run tests from GOPATH
    - no longer need to set GOPATH

--- ci/tasks/run-tests/task.sh
+++ ci/tasks/run-tests/task.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -exu
 
 ROOT=${PWD}
-GOPATH=${PWD}/go
-PATH=${GOPATH}/bin:$PATH
 
 function main() {
   apt-get update
   apt-get install -yyq zip
   go get github.com/onsi/ginkgo/ginkgo
 
-  mkdir -p "${GOPATH}/src/github.com/pivotal-cf"
-  ln -s "${ROOT}/azure-blobstore-resource" "${GOPATH}/src/github.com/pivotal-cf/azure-blobstore-resource"
-  ginkgo -r "${GOPATH}/src/github.com/pivotal-cf/azure-blobstore-resource"
+  pushd "${ROOT}/azure-blobstore-resource" > /dev/null
+    ginkgo -r .
+  popd > /dev/null
 }
 
 main
